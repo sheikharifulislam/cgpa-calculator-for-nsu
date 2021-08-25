0a87e40116321f9fca473de3ef76d28fb526ad47
sheikh ariful islam <sheikharifulisalm770@gmail.com>
2021-08-25 23:08:50 +0600

    project end

--- js/index.js
+++ js/index.js
@@ -1,40 +1,40 @@
 //add child
 
 function addtask() {
-    const newTask = `<div class="task">
-    <div class="course-name">
-        <input type="text" placeholder="Course">
-    </div>
-    <div class="credits">
-        <select name="" id="" value="0">
-            <option value="">3</option>
-            <option value="">1</option>
-            <option value="0" selected>0</option>
-            <option value="">1.5</option>
-            <option value="">2</option>
-            <option value="">4</option>
-        </select>
-    </div>
-    <div class="grade">
-        <select name="" id="">
-             <option value="">A</option>
-            <option value="">A-</option>
-            <option value="">B+</option>
-            <option value="">B</option>
-            <option value="">B-</option>
-            <option value="">C+</option>
-            <option value="">C</option>
-            <option value="">C-</option>
-            <option value="">D+</option>
-            <option value="">D</option>
-            <option value="">F</option>
-        </select>
-    </div>
-    <div class="close">
-        <i class="far fa-times-circle"></i>
-    </div>
-</div>`;
-    document.getElementById('tasks').insertAdjacentHTML('beforebegin', newTask);
+    const newTask = document.createElement('div')
+    newTask.classList.add('task');
+    newTask.innerHTML = `<div class="course-name">
+    <input type="text" placeholder="Course">
+</div>
+<div class="credits">
+    <select name="" id="" value="0">
+        <option value="">3</option>
+        <option value="">1</option>
+        <option value="0" selected>0</option>
+        <option value="">1.5</option>
+        <option value="">2</option>
+        <option value="">4</option>
+    </select>
+</div>
+<div class="grade">
+    <select name="" id="">
+         <option value="">A</option>
+        <option value="">A-</option>
+        <option value="">B+</option>
+        <option value="">B</option>
+        <option value="">B-</option>
+        <option value="">C+</option>
+        <option value="">C</option>
+        <option value="">C-</option>
+        <option value="">D+</option>
+        <option value="">D</option>
+        <option value="">F</option>
+    </select>
+</div>
+<div class="close">
+    <i class="far fa-times-circle"></i>
+</div>`
+    document.getElementById('tasks').appendChild(newTask);
 }
 
 document.querySelector('.add-task-btn .fa-plus-circle').addEventListener('click',
@@ -46,11 +46,9 @@ document.querySelector('.add-task-btn .fa-plus-circle').addEventListener('click'
 //remove child
 
 let allTasks = document.getElementById('tasks');
-
-const removeButons = document.getElementsByClassName('fa-times-circle');
-for (const removeButon of removeButons) {
-    removeButon.addEventListener('click', function(e) {
+allTasks.addEventListener('click', function(e) {
+    if (e.target.classList.value.includes('fa-times-circle')) {
         const task = e.target.parentElement.parentElement;
         allTasks.removeChild(task);
-    });
-}
+    }
+})
